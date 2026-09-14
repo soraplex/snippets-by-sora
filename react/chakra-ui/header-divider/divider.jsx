6a7b1d06862479ba0e5297ecc54c92f2f1c83a83
scott <scottshiro@outlook.com>
2026-09-14 02:21:08 -0400

    add header divider line template to react folder

---
+++ react/chakra-ui/header-divider/divider.jsx
@@ -0,0 +1,14 @@
+// Header divider line
+
+<Flex align="center" mb={6}>
+  <Heading size="md" color="accent">
+    Projects
+  </Heading>
+
+  <Divider
+    ml={4}
+    mt={1}
+    flex="1"
+    borderColor="" // your theme token or any color
+  />
+</Flex>;
